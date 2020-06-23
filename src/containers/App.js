6b1bc22a5a230632c7ebc0d1c5c2f59cf1af32ae
Dawn Ann <heartandhandstraining@gmail.com>
2020-06-23 22:03:15 +0000

    add react-redux Provider, connect

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,30 @@
 import React, {Component } from 'react';
+import { connect } from 'react-redux';
 import Cardlist from '../components/Cardlist';
 import SearchBox from '../components/SearchBox'
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
 import '../containers/App.css';
 
+import {setSearchField } from '../actions'
+
+const mapStateToProps = state => {
+	return {
+		searchField: state.searchField
+	}
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+	}
+}
+	  
 class App extends Component {
 	constructor() {
 		super()
 		this.state = {
 			robots: [],
-			searchfield: ''
 		}
 	}
 	
@@ -21,14 +35,11 @@ class App extends Component {
 		.then(users => this.setState({ robots: users }));
 		}
 
-	onSearchChange = (event) => {
-		this.setState({searchfield: event.target.value})
-	}
-	
 	render() {
-		const { robots, searchfield } = this.state;
+		const { robots } = this.state;
+		const { searchField, onSearchChange } = this.props;
 		const filteredRobots = robots.filter(robot => {
-			return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+			return robot.name.toLowerCase().includes(searchField.toLowerCase());
 			})
 		
 		return !robots.length ?
@@ -36,7 +47,7 @@ class App extends Component {
 			(
 				<div className='tc'>
 					<h1>RoboFriends</h1>
-					<SearchBox searchChange={this.onSearchChange} />
+					<SearchBox searchChange={onSearchChange} />
 					<Scroll>
 						<ErrorBoundary>
 							<Cardlist robots = {filteredRobots} />
@@ -47,4 +58,4 @@ class App extends Component {
 		}
 	}
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
